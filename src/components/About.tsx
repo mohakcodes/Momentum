5d7edadee7c19c8125e5f117d2eb0eca9017721f
Mohak Chauhan <mohak46812@gmail.com>
2024-10-21 23:51:02 +0530

    init: about page

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='font-semibold flex flex-col gap-2'>
+        <h1 className='text-4xl'>About</h1>
+        <button className='bg-green-500 rounded-md'><a href="/auth/signup">Test</a></button>
+    </div>
+  )
+}
+
+export default About
